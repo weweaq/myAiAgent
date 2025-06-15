f0e6e80709e38993c57ea435d5f8ffcf89d74b08
weweaq <1773465183@qq.com>
2025-06-15 14:34:04 +0800

    第三章 添加chatMemory

--- src/main/java/com/example/myaiagent/app/LoveApp.java
+++ src/main/java/com/example/myaiagent/app/LoveApp.java
@@ -2,6 +2,7 @@ package com.example.myaiagent.app;
 
 import com.example.myaiagent.advisor.MyLoggerAdvisor;
 import com.example.myaiagent.advisor.ReReadingAdvisor;
+import com.example.myaiagent.chatmemory.FileBasedChatMemory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
@@ -10,6 +11,8 @@ import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY;
 import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_RETRIEVE_SIZE_KEY;
@@ -27,7 +30,12 @@ public class LoveApp {
 
     public LoveApp(ChatModel dashscopeChatModel) {
         // 初始化基于内存的对话记忆
-        ChatMemory chatMemory = new InMemoryChatMemory();
+//        ChatMemory chatMemory = new InMemoryChatMemory();
+
+                // 初始化基于文件的对话记忆
+        String fileDir = System.getProperty("user.dir") + "/tmp/chat-memory";
+        ChatMemory chatMemory = new FileBasedChatMemory(fileDir);
+
         chatClient = ChatClient.builder(dashscopeChatModel)
                 .defaultSystem(SYSTEM_PROMPT)
                 .defaultAdvisors(
@@ -50,4 +58,19 @@ public class LoveApp {
         return content;
     }
 
+    public LoveReport doChatWithReport(String message, String chatId) {
+        LoveReport loveReport = chatClient
+                .prompt()
+                .system(SYSTEM_PROMPT + "每次对话后都要生成恋爱结果，标题为{用户名}的恋爱报告，内容为建议列表")
+                .user(message)
+                .advisors(spec -> spec.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId)
+                        .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 10))
+                .call()
+                .entity(LoveReport.class);
+        log.info("loveReport: {}", loveReport);
+        return loveReport;
+    }
+
+    public record LoveReport(String title, List<String> suggestions) {
+    }
 }
--- src/test/java/com/example/myaiagent/LoveAppTest.java
+++ src/test/java/com/example/myaiagent/LoveAppTest.java
@@ -30,4 +30,41 @@ class LoveAppTest {
         answer = loveApp.doChat(message, chatId);
         Assertions.assertNotNull(answer);
     }
+
+    @Test
+    void doChatWithReport() {
+        String chatId = UUID.randomUUID().toString();
+        // 第一轮
+        String message = "你好，我是程序员鱼皮，我想让另一半（编程导航）更爱我，但我不知道该怎么做";
+        LoveApp.LoveReport loveReport = loveApp.doChatWithReport(message, chatId);
+        Assertions.assertNotNull(loveReport);
+    }
+
+    @Test
+    void doChatWithReportCheck() {
+        String chatId = UUID.randomUUID().toString();
+        // 第一轮
+        String message = "你好，我是程序员鱼皮";
+        LoveApp.LoveReport answer = loveApp.doChatWithReport(message, chatId);
+        Assertions.assertNotNull(answer);
+        // 第二轮
+        message = "我想让另一半（编程导航）更爱我";
+        answer = loveApp.doChatWithReport(message, chatId);
+        Assertions.assertNotNull(answer);
+        // 第三轮
+        message = "我的另一半叫什么来着？刚跟你说过，帮我回忆一下";
+        answer = loveApp.doChatWithReport(message, chatId);
+        Assertions.assertNotNull(answer);
+    }
+
+    /**
+     * 测试继续聊天 "fc906b2c-e3aa-4067-bfbd-06e06518f2b0"
+     */
+    @Test
+    void doChatWithReportWithContinueChat() {
+        String message =  "我的另一半叫什么来着？刚跟你说过，帮我回忆一下";
+        LoveApp.LoveReport answer = loveApp.doChatWithReport(message, "fc906b2c-e3aa-4067-bfbd-06e06518f2b0");
+        Assertions.assertNotNull(answer);
+    }
+
 }
